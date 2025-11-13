0d8332e70e82d25a091c33f75a069d34a3093a76
ana-andari <septianaandari99@guru.smk.belajar.id>
2025-11-13 09:25:54 +0700

    Code Review
    Review

---
+++ Assets/Scripts/12Nov25/Mas Em/SquareMove.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class SquareMove : MonoBehaviour
+{
+
+    public bool isMoveRight = true;
+    public float kecepatan = 5.0f;
+    public bool isMove = true;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isMove == true)
+        {
+            if (isMoveRight == true)
+            {
+                // gerak ke kanan
+                gameObject.transform.Translate(kecepatan * Time.deltaTime, 0, 0);
+            }
+            else
+            {
+                // gerak ke kiri
+                gameObject.transform.Translate(-kecepatan * Time.deltaTime, 0, 0);
+            }
+        }
+        else
+        {
+            gameObject.transform.Translate(0, 0, 0);
+        }
+    }
+
+
+}
